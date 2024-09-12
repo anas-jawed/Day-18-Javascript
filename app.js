7ed0b462ba1360b600fd11f9001ffc3e2c902565
Anas Jawed <anasjawedbmj@gmail.com>
2024-09-12 16:19:14 +0500

    Day 18

---
+++ app.js
@@ -0,0 +1,155 @@
+// JS OBJECT LITERSLS
+
+// cnost biodata = {
+//     name: "Anas",
+//     fname: "Jawaid",
+//     age: 23,
+//     education: ["bachelor","intermediate"]
+// };
+// console.log(biodata);
+
+
+//  Thread / Twitter Post
+// create an object literal for the properties of thread /tiwitter post which includes
+// .username
+// .content
+// .likes
+// .repost
+// .tags
+
+// const post = {
+// username: "@anasjawaid",
+// content: "This is my #first post",
+// like: 150,
+// repost: 10,
+// tags: ['apnacollege', 'delta']
+// };
+
+//  GEt values
+
+// let students =  {
+//  name: "ANas",
+//  marks: 78,
+//  age: 23,
+// };
+
+// let prop = "name";
+// console.log(students[prop]);
+
+//  Comparison
+
+// const obj = {
+//     1: "a",
+//     2: "b",
+//     true: "c",
+//     null: "d",
+//     undefined: "e"
+// };
+// console.log(obj);
+
+
+//             ADD Update or delete
+
+// const students = {
+//     name: "Anas Jawaid",
+//     age: 23,
+//     marks: {
+//         english: 84,
+//         maths: 87,
+//         physcis: 78,
+//         urdu: 88,
+//         chemistry: 98
+//     },
+//     education: {
+//         school: "V.M Public SChool",
+//         college: "PECHS GOVT boys college karachi",
+//         university: "Sindh madressatul Islam University karachi,"
+//     },
+//     religion: "islam"
+// };
+
+// console.log(students);
+
+
+// const classinfo = {
+//   student1: {
+//     name: "Anas Jawaid",
+//     age: 23,
+//     marks: 87,
+//     city: "karachi"
+//   },
+//   student2: {
+//     name: "suhaib",
+//     age: 24,
+//     marks: 89,
+//     city: "karachi"
+//   },
+//   students3: {
+//     name: "saad",
+//     age: 23,
+//     marks: 84,
+//     city: "karachi"
+//   }
+// };
+
+// classinfo.student2.city = "hyderabad";
+
+// console.log(classinfo)
+
+//    Array Of Objects
+
+
+// const classinfo = [
+//    {
+// name: "Anas Jawaid",
+// age: 23,
+// marks: 87,
+// city: "karachi"
+//   },
+//   {
+//     name: "Suhaib Usman",
+// age: 23,
+// marks: 89,
+// city: "hyederabad"
+//   },
+//   {
+//     name: "Saad ALi",
+// age: 23,
+// marks: 84,
+// city: "Larkana"
+//   }
+// ];
+
+// console.log(classinfo[0].gender = "Male");
+
+// console.log(classinfo[1].gender = "Male");
+// console.log(classinfo[2].gender = "Male");
+// console.log(delete classinfo[2].gender);
+// console.log(classinfo);
+
+
+
+//  Maths Objects
+// let step = Math.random();
+// console.log(step);
+// let step1 = step * 10;
+// console.log(step1);
+// let step2 = Math.floor(step1);
+// console.log(step2);
+
+
+// let random = Math.floor(Math.random() * 10) + 1;
+// console.log(random);
+
+// let random = Math.floor(Math.random() * 100) + 70;
+// console.log(random);
+
+// let num1 = Math.floor(Math.random() * 5) + 1;
+// console.log(num1);
+
+// const min = 70;
+// const max = 100;
+
+// const randomnumer = Math.floor(Math.random() * (max - min + 1) + min);
+// console.log(randomnumer ) 
+
